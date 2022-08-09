a8e086a4e4708ba070318c00af21178d1dc0ba55
fafomax <leonardodmora@gmail.com>
2022-08-09 20:37:38 -0300

    Ya terminado el carrito, falta el search

--- js/init.js
+++ js/init.js
@@ -21,6 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
     constrainWidth: false,
     constrainHeit: false,
     closeOnClick: false,
+    inDuration: 700,
+    outDuration: 900,
   });
 });
 
